d949aca5a41c232abbc2ad26f6cfb540112e0fdb
Adam46072 <adam.bartl2001@seznam.cz>
2023-09-25 17:18:40 +0200

    water mater and contact person complete

--- PVK/ContactPerson.cs
+++ PVK/ContactPerson.cs
@@ -8,5 +8,20 @@ namespace PVK
 {
     internal class ContactPerson
     {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public long telephoneNumber { get; private set; }
+        private Billing billing { get; set; }
+        public ContactPerson(string name, string surname, long telephoneNumber)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.telephoneNumber = telephoneNumber;
+        }
+        public string RecieveBill(Billing billing)
+        {
+            this.billing = billing;
+            return string.Format("{0}",billing.price);
+        }
     }
 }
--- PVK/WaterMeter.cs
+++ PVK/WaterMeter.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace PVK
 {
-    internal class WaterMater
+    internal class WaterMeter
     {
+        public string Label { get; init; }
+        public int consumedValue { get; set; }
+        public WaterMeter(string label)
+        {
+            this.Label = label;
+        }
+        public void WaterCharges()
+        {
+            consumedValue += 1;
+        }
+        public override string ToString()
+        {
+            return consumedValue.ToString();
+        }
+
     }
 }
