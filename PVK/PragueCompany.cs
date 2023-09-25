39f8d912fd8554ee684e7089713f9b31c0ac35b2
Adam46072 <adam.bartl2001@seznam.cz>
2023-09-25 17:19:41 +0200

    company almost done

--- PVK/PragueCompany.cs
+++ PVK/PragueCompany.cs
@@ -8,5 +8,38 @@ namespace PVK
 {
     internal class PragueCompany
     {
+        public string nameCompany { get; set; }
+        private List<object> realEstate { get; set; }
+        public PragueCompany(string nameCompany)
+        {
+            this.nameCompany = nameCompany;
+            realEstate = new List<object>();
+        }
+        public string BillingTime()
+        {
+            string bill = "";
+            foreach (object item in realEstate)
+            {
+                if(item is House)
+                {
+                    House house = (House)item;
+                    bill += house.contactPerson.RecieveBill(new Billing(house.contactPerson.Name, house.contactPerson.Surname, house.contactPerson.telephoneNumber, house.waterMater.consumedValue, house.waterMater.consumedValue * 20)) + "\n";
+                }
+                if(item is ApartmentBuilding)
+                {
+                    ApartmentBuilding apartment = (ApartmentBuilding)item;
+                    foreach (Flat flats in apartment.flats)
+                    {
+                        bill += flats.contactPerson.RecieveBill(new Billing(flats.contactPerson.Name, flats.contactPerson.Surname, flats.contactPerson.telephoneNumber, flats.waterMater.consumedValue, flats.waterMater.consumedValue * 20)) + "\n";
+                    }
+                }
+            }
+            return bill;
+        }
+        public void AddToList(object realEstate)
+        {
+            this.realEstate.Add(realEstate);
+        }
+
     }
 }
