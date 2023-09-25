21132a5a61f4289b89b6120cf26baf9bbd00b4e8
Adam46072 <adam.bartl2001@seznam.cz>
2023-09-25 17:17:53 +0200

    complete bill

--- PVK/Billing.cs
+++ PVK/Billing.cs
@@ -8,5 +8,20 @@ namespace PVK
 {
     internal class Billing
     {
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public long telephoneNumber { get; private set; }
+        public int consumedWater { get; init; }
+        public const int priceForLitre = 20;
+        public int price { get; init; }
+        public Billing(string name, string surname, long telephoneNumber, int consumedWater, int price)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.telephoneNumber = telephoneNumber;
+            this.consumedWater = consumedWater;
+            this.price = price;
+
+        }
     }
 }
